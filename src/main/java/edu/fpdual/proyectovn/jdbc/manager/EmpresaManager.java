5d62f81ad41aed18c547f43fd33d1da7154932d3
Veronica Gonzalez Pons <veronicagnzpns@gmail.com>
2022-05-09 08:48:12 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/webapp/index.jsp

--- src/main/java/edu/fpdual/proyectovn/jdbc/manager/EmpresaManager.java
+++ src/main/java/edu/fpdual/proyectovn/jdbc/manager/EmpresaManager.java
@@ -1,4 +1,23 @@
 package edu.fpdual.proyectovn.jdbc.manager;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public interface EmpresaManager {
+
+    /**
+     * Busca todos las empresas
+     * @param con conexión a BBDD
+     * @return {@code Set} of {@code Usuario}
+     */
+    public ResultSet TodosEmpresas(Connection con) throws SQLException;
+
+    /**
+     * Busca todos los usuarios
+     * @param con conexión a BBDD
+     * @return {@code Set} of {@code Usuario}
+     */
+    public ResultSet ModificarEmpresa (Connection con, String nomEmp, int idCiu, int idEmp ) throws SQLException;
 }
